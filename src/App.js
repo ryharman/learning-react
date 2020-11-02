8620bbf3c056762020de290f7d6f2982c2beca10
Ryan Harman <ryan.harman63@gmail.com>
2020-11-02 15:11:56 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckSquare, faEdit } from '@fortawesome/free-solid-svg-icons';
 import TodoForm from './Components/TodoForm';
 
-library.add(faCheckSquare, faEdit);
 
 function App() {
   return (
--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -2,8 +2,7 @@ import React, { useReducer } from "react";
 import '../App.css';
 import { todoReducer } from "../todoReducer";
 import TodoItem from "./TodoItem";
-import TextField from '@material-ui/core/TextField';
-import { Divider, Typography } from "@material-ui/core";
+import { Divider, Typography, TextField, Card, CardContent, } from "@material-ui/core";
 
 function initialValues() {
   if(JSON.parse(localStorage.getItem('todo')) === null) {
@@ -38,50 +37,60 @@ export default function TodoForm() {
 
   return (
     <div className="listContent">
-      <Typography variant="h4"> Tasks </Typography>
-      <Divider></Divider>
-      <ul>
-        {getUnfinishedTasks(todoState).map((task) => (
-          <TodoItem 
-          task={task} 
-          onChange={() =>
-            dispatch({ type: "TOGGLE", payload: { taskId: task.id } })
+      <div className="addTask">
+        <TextField 
+        name="task"
+        label="Add new task"
+        id="newTaskId"
+        onKeyPress={e => {
+          if(e.key === 'Enter') {
+            dispatch({ type: "ADD", payload: document.getElementById("newTaskId").value})
+            // Resets the input fields value
+            document.getElementById("newTaskId").value = "";
           }
-          onDelete={() =>
-            dispatch({ type: "DELETE", payload: { taskId: task.id } })
-          }
-          />
-        ))}
-      </ul>
-      <TextField 
-      name="task"
-      label="Add new item"
-      id="newTaskId"
-      onKeyPress={e => {
-        if(e.key === 'Enter') {
-          dispatch({ type: "ADD", payload: document.getElementById("newTaskId").value})
-          // Resets the input fields value
-          document.getElementById("newTaskId").value = "";
-        }
-      }}
-      />
-      <div className="completed">
-        <Typography variant="h4"> Completed Tasks </Typography>
-        <Divider></Divider>
-        <ul>
-          {getFinishedTasks(todoState).map((task) => (
-            <TodoItem 
-            task={task} 
-            onChange={() =>
-              dispatch({ type: "TOGGLE", payload: { taskId: task.id } })
-            }
-            onDelete={() =>
-              dispatch({ type: "DELETE", payload: { taskId: task.id } })
-            }
-            />
-          ))}
-        </ul>
+        }}/>
       </div>
+      
+      <Card>
+        <CardContent>
+          <Typography variant="h4"> Tasks </Typography>
+          <Divider></Divider>
+          <ul>
+            {getUnfinishedTasks(todoState).map((task) => (
+              <TodoItem 
+              task={task} 
+              onChange={() =>
+                dispatch({ type: "TOGGLE", payload: { taskId: task.id } })
+              }
+              onDelete={() =>
+                dispatch({ type: "DELETE", payload: { taskId: task.id } })
+              }
+              />
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+
+      <Card className="completed">
+        <CardContent>
+          <Typography variant="h4"> Completed Tasks </Typography>
+          <Divider></Divider>
+          <ul>
+            {getFinishedTasks(todoState).map((task) => (
+              <TodoItem 
+              task={task} 
+              onChange={() =>
+                dispatch({ type: "TOGGLE", payload: { taskId: task.id } })
+              }
+              onDelete={() =>
+                dispatch({ type: "DELETE", payload: { taskId: task.id } })
+              }
+              />
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+      
     </div>
   );
 }
